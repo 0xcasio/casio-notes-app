18fef52b5963b2bbd8af51937beec9401d97ecfe
marcos <defipm@protonmail.com>
2025-03-22 12:00:41 -0400

    add small changes to hero page

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,11 +43,8 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Team Task Manager</Link>
-                    <Link href={"/protected/tasks"} className="text-sm">My Tasks</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
+                    <Link href={"/"}>Home</Link>
+                    <Link href={"/protected/tasks"} className="text-sm">My Tasks</Link>                    
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
